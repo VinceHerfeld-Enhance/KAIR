e657e2a02af70765f91dd6b4c137bad67ffeabcb
vherfeld <vincent.herfeld@inria.fr>
2026-07-07 18:46:35 +0200

    ag_accretion

--- models/select_network.py
+++ models/select_network.py
@@ -334,6 +334,7 @@ def define_G(opt):
             window_motion_mode=opt_net.get("window_motion_mode", "compose"),
             shared_onesided_mode=opt_net.get("shared_onesided_mode", "qvi"),
             ag_shared_cues=opt_net.get("ag_shared_cues", True),
+            ag_accretion_deletion=opt_net.get("ag_accretion_deletion", False),
             bspline_confidence_splat=opt_net.get("bspline_confidence_splat", False),
             bspline_window_coupling=opt_net.get("bspline_window_coupling", False),
             bspline_coupling_hidden=opt_net.get("bspline_coupling_hidden", 64),
